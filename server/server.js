d99b3b022b480d6a3fd9b58eafe3dbfdee6b39e2
Abhishek Kanojiya <abhishekkanojia913@gmail.com>
2026-09-15 22:29:57 +0530

    Update server port for deployment

--- server/server.js
+++ server/server.js
@@ -99,7 +99,7 @@ app.put("/orders/:id", (req, res) => {
 });
 
 // Start Server
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
